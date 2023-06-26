1de7ee6a62e45a9baf29fec87b1a17c987e24c76
Winnie Koum <wkoumwonkoagni@myges.fr>
2023-06-26 12:15:02 +0200

    mise en place des switch case

--- ProjetPizzaGroupe6/Program.cs
+++ ProjetPizzaGroupe6/Program.cs
@@ -9,35 +9,38 @@ public class Program
         Console.WriteLine("Pizzas disponibles :");
         Console.WriteLine("#####################################");
         orderProcessor.DisplayAvailablePizzas();
+
         while (true)
         {
             Console.WriteLine("\n#####################################\n");
-            Console.WriteLine("Comment souhaitez vous renseigner votre commande (ou 'exit' pour quitter) :");
+            Console.WriteLine("Comment souhaitez-vous renseigner votre commande (ou 'exit' pour quitter) :");
             Console.WriteLine("Saisissez '1' pour saisir la commande");
             Console.WriteLine("Saisissez '2' pour charger le fichier des commandes");
-            var input = Console.ReadLine();
 
-            if (input.ToLower() == "exit")
+            var input = Console.ReadLine().ToLower();
+
+            if (input == "exit")
                 break;
 
-            if (input.ToLower() == "1")
+            switch (input)
             {
-                Console.WriteLine("Entrez une commande (ou 'exit' pour quitter) :");
-                input = Console.ReadLine();
-                orderProcessor.ProcessOrder(input);
-                if (input.ToLower() == "exit")
+                case "1":
+                    Console.WriteLine("Entrez une commande (ou 'exit' pour quitter) :");
+                    input = Console.ReadLine();
+                    orderProcessor.ProcessOrder(input);
                     break;
-            }
 
-            if (input.ToLower() == "2")
-            {
-                Console.WriteLine("Entrez le chemin du fichier contenant les commandes :");
-                var filePath = Console.ReadLine();//C:\Users\winnie\Documents\ETUDES\LAB\MASTER 1\Semestre 2\Design patterns\projet\json.json
-                orderProcessor.VerifyFilePath(filePath);//2 Regina, 1 4 Saisons, 3 Végétarienne
+                case "2":
+                    Console.WriteLine("Entrez le chemin du fichier contenant les commandes :");
+                    var filePath = Console.ReadLine();
+                    orderProcessor.VerifyFilePath(filePath);
+                    break;
 
-                if (filePath.ToLower() == "exit")
+                default:
+                    Console.WriteLine("Choix invalide. Veuillez réessayer.");
                     break;
             }
         }
     }
+
 }
