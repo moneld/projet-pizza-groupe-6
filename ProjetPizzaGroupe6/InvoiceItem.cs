6d8e50260b43c2e5a6297bb22f12402b3acc7ea7
Monel.D <monelyehoun@gmail.com>
2023-06-26 20:04:43 +0200

    Modification du fichier invoiceitem

--- ProjetPizzaGroupe6/InvoiceItem.cs
+++ ProjetPizzaGroupe6/InvoiceItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
-
-namespace ProjetPizzaGroupe6
+﻿namespace ProjetPizzaGroupe6
 {
     public class InvoiceItem
     {
